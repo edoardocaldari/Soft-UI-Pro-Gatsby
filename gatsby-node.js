377e52d647d1eebf9d2bc0402af2582da671e4cd
edoardocaldari <125738879+edoardocaldari@users.noreply.github.com>
2023-02-21 15:40:55 +0100

    Commit from TeleportHQ

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,9 +1,1 @@
 /* gatsby-node.js */
-const fs = require("fs");
-exports.onPostBuild = () => {
-  fs.copyFile(`./firebase.json`, `./public/firebase.json`, (err) => {
-    if (err) {
-      throw err;
-    }
-  });
-};
