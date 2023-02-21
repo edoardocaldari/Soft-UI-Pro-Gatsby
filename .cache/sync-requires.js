98d3c2a07e6856bcd9f8b9dace8b309efad930a5
edoardocaldari <125738879+edoardocaldari@users.noreply.github.com>
2023-02-21 15:59:45 +0100

    ultima prova

---
+++ .cache/sync-requires.js
@@ -0,0 +1,12 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("C:\\Users\\User\\Documents\\GitHub\\Soft-UI-Pro-Gatsby\\.cache\\dev-404-page.js")),
+  "component---src-pages-coming-soon-js": preferDefault(require("C:\\Users\\User\\Documents\\GitHub\\Soft-UI-Pro-Gatsby\\src\\pages\\coming-soon.js")),
+  "component---src-pages-index-js": preferDefault(require("C:\\Users\\User\\Documents\\GitHub\\Soft-UI-Pro-Gatsby\\src\\pages\\index.js")),
+  "component---src-pages-profile-js": preferDefault(require("C:\\Users\\User\\Documents\\GitHub\\Soft-UI-Pro-Gatsby\\src\\pages\\profile.js"))
+}
+
